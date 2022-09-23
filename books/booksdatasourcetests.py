02941a828172c4f2873ab4a3eda883c739258aae
Luca Dantas <48097784+LucaDantas@users.noreply.github.com>
2022-09-23 12:19:09 -0300

    Added unit tests to test the books between years query

--- books/booksdatasourcetests.py
+++ books/booksdatasourcetests.py
@@ -26,6 +26,43 @@ class BooksDataSourceTester(unittest.TestCase):
         self.assertTrue(books[1].title == 'Neverwhere')
         self.assertTrue(books[2].title == 'Omoo')
 
+    def test_wrong_input_type_to_years(self):
+        self.assertRaises(TypeError, self.books_between_years, 1920, [1940])
+        self.assertRaises(TypeError, self.books_between_years, [], [])
+        self.assertRaises(TypeError, self.books_between_years, "bom dia", 1940)
+        self.assertRaises(TypeError, self.books_between_years, "asd", [])
+
+    def test_all_books_years(self):
+        self.assertEqual(len(self.books_between_years), len(self.data_source))
+
+    def test_no_books_years(self):
+        self.assertEqual(len(self.books_between_years(100000, 100001), 0))
+
+    def test_inverted_range_years(self):
+        self.assertEqual(len(self.books_between_years(1000000, 0), 0))
+
+    def test_sorted_by_year(self):
+        tiny_data_source = BooksDataSource('tinybooks.csv')
+        books = tiny_data_source.books_beween_years()
+        self.assertTrue(len(books) == 3)
+        self.assertTrue(books[0].title == 'Emma')
+        self.assertTrue(books[1].title == 'Omoo')
+        self.assertTrue(books[2].title == 'Neverwhere')
+
+    def test_sorted_tied_years(self):
+        books = self.books_between_years(1982, 1987)
+        assertEqual(books[0].title, 'A Wild Sheep Chase')
+        assertEqual(books[1].title, 'Hard-Boiled Wonderland and the End of the World')
+        assertEqual(books[2].title, 'Love in the Time of Cholera')
+        assertEqual(books[3].title, 'Shards of Honor')
+        assertEqual(books[4].title, 'Beloved')
+
+    def test_years_no_ending(self):
+        assertEqual(self.books_between_years(1995), self.books_between_years(1995, 100000))
+
+    def test_years_no_beginning(self):
+        assertEqual(self.books_between_years(None, 1990), self.books_between_years(-100000, 1990))
+    
 if __name__ == '__main__':
     unittest.main()
 
